084c39920f16d56f4bbaf53bead411ec5271f8a3
CangarejoAzul <102051174+CangarejoAzul@users.noreply.github.com>
2022-07-24 19:27:06 +0100

    Update untranslated.py

--- untranslated.py
+++ untranslated.py
@@ -1,9 +1,7 @@
+from sys import argv
 from re import findall
 
-source = "deu"
-target = "eng"
-
-input = open("links.csv")
+input = open(argv[3], encoding="utf-8")
 adjacency = {}
 print("Reading links...")
 for line in input:
@@ -12,52 +10,38 @@ for line in input:
     adjacency[split[0]] = []
   adjacency[split[0]].append(split[1])
 
-input = open("sentences.csv")
-output = open("adjacency.csv", "w")
-print("Writing unlinked...")
+input = open(argv[4], encoding="utf-8")
+output = open(argv[5], "w")
+print("Writing adjacency...")
 for line in input:
   split = findall("[^\t\n]+", line)
-  if split[0] not in adjacency:
-    print(split[0], file = output)
-
-print("Writing linked...")
-for id1 in adjacency:
-  print(id1, end = "", file = output)
-  for id2 in adjacency[id1]:
-    print("\t", id2, sep = "", end = "", file = output)
-  for id2 in adjacency[id1]:
-    for id3 in adjacency[id2]:
-      print("\t", id3, sep = "", end = "", file = output)
+  print(split[0], end = "", file = output)
+  if split[0] in adjacency:
+    for id1 in adjacency[split[0]]:
+      print("\t", id1, sep = "", end = "", file = output)
+      for id2 in adjacency[id1]:
+        print("\t", id2, sep = "", end = "", file = output)
   print("\n", end = "", file = output)
 
 del adjacency
 
-input = open("sentences.csv", encoding="utf-8")
+input = open(argv[4], encoding="utf-8")
 language = {}
 print("Reading languages...")
 for line in input:
   split = findall("[^\t\n]+", line)
   language[split[0]] = split[1]
 
-input = open("adjacency.csv")
-output = open("ids.csv", "w")
-print("Writing IDs...")
-for line in input:
-  split = findall("[^\t\n]+", line)
-  if split[0] in language and language[split[0]] == source and all(id not in language or language[id] != target for id in split):
-    print(split[0], file = output)
-
-del language
-
+input = open(argv[5], encoding="utf-8")
 untranslated = set()
-input = open("ids.csv")
-print("Reading IDs...")
+print("Finding untranslated...")
 for line in input:
   split = findall("[^\t\n]+", line)
-  untranslated.add(split[0])
+  if split[0] in language and language[split[0]] == argv[1] and all(id not in language or language[id] != argv[2] for id in split):
+    untranslated.add(split[0])
 
-input = open("sentences.csv", encoding="utf-8")
-output = open("untranslated.csv", "w", encoding="utf-8")
+input = open(argv[4], encoding="utf-8")
+output = open(argv[6], "w", encoding="utf-8")
 print("Writing untranslated...")
 for line in input:
   split = findall("[^\t\n]+", line)
